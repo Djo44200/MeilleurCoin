8f3421c9a6b24e3bfc82ca68d594dc19d96df990
jbodet2019 <jonathan.bodet2019@campus-eni.fr>
2019-08-19 14:48:01 +0200

    Commit

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,14 +8,39 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AccueilController
+ * @package App\Controller
+ * @Route("")
+ */
+
 class AccueilController extends Controller
 {
 
     /**
      * @Route("", name="accueil", methods={"GET"})
+     * @Route("home", name="home", methods={"GET"})
      */
     public function accueil() {
 
         return $this->render('Accueil/accueil.html.twig');
+    }
+
+    /**
+     * @Route("cgu", name="CGU", methods={"GET"})
+     */
+
+    public function cgu(){
+
+        return $this->render('Accueil/cgu.html.twig');
+    }
+
+    /**
+     * @Route("faq", name="FAQ", methods={"GET"})
+     */
+
+    public function faq(){
+
+        return $this->render('Accueil/faq.html.twig');
     }
 }
